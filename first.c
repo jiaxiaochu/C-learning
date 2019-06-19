e262a38f48a7f128e90d175e29500d59f2881c98
jiaxiaochu <jiaxiaochu@gmail.com>
2019-06-19 23:58:26 +0800

    create first C programer

---
+++ first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])	// 一个简单的c程序
+{
+	int num;	// 定义一个名为num的变量
+	num = 1;	// 为num赋一个值
+
+	// printf("%s\n", );
+	printf("I am a simple");	// 使用printf()函数
+	printf("computer.\n");
+	printf("My favorite number is %d because it is first.\n", num);	
+
+	return 0;
+}
