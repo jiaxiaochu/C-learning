6c55b6fa465611cc3a8ed068918115bfbf607031
Mr.jia <jiaxiaochu@gmail.com>
2019-09-19 23:30:05 +0800

    Create dd.c
    练习

---
+++ dd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+	int num;
+	num = 1;
+	printf("I am a simple\n");
+	printf("computer.\n");
+	printf("My faviorite number is %d because it is first.\n", num );
+	return 0;
+}
